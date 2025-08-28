157a7ae0afca5da11241362b1aaddf3881f5c02a
swathigopavarapu <41499776+swathigopavarapu@users.noreply.github.com>
2025-08-28 22:36:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault();
+	const firstName = document.querySelector("input[name='fname']").value.trim();
+	const lastName  = document.querySelector("input[name='lname']").value.trim();
+	const fullName = firstName + " " + lastName;
+	alert(fullName);
+	
+
 
 }
